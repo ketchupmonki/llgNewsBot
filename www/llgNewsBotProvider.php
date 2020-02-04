33303346e04d6a9b97279ec7a2c41c8403f83990
Alexander Theulings <alextheulings@gmail.com>
2020-02-04 21:46:53 +0000

    Added missing license information.

--- www/llgNewsBotProvider.php
+++ www/llgNewsBotProvider.php
@@ -1,4 +1,24 @@
 <?php
+/*
+llgNewsBotProvider.php
+Copyright (C) 2020 Alexander Theulings, ketchupcomputing.com <alexander@theulings.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+Documentation for this file can be found at https://ketchupcomputing.com/llg-news-bot/
+*/
+
 $pd = json_decode(file_get_contents('php://input'));
 //Make sure the required json pairs have been provided
 if (!isset($pd->username) || !isset($pd->password) || !isset($pd->requestType)){
